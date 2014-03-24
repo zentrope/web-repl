6e2be71e086f6e9005962d4cddd41151da6c6e97
Keith Irwin <keith@zentrope.com>
2014-03-23 21:35:41 -0700

    Reorg to support being a maven artifact.

--- README.md
+++ README.md
@@ -1,16 +1,64 @@
 # web-repl
 
-Experiment. Can I create a small lib that you include and then start
-in your app such that you can get a REPL going via a web browser?
+An embeddable JVM-hosted web-app enabling a JVM/Clojure repl in a
+browser.
 
-The idea is that you start up your back-end application (Clojure on
-the JVM). Then hit a web page and you can access the app via a REPL in
-a browser.
+The idea is that as part of your start up of your back-end application
+(Clojure on the JVM), you also start up a Web REPL service. You then
+hit the web page for the port you configured and you can access the
+app via a REPL in a browser.
 
 Why?
 
 So you can demo stuff and inspect stuff, say, in a review meeting of
 some sort.
+
+## Usage
+
+Add the following as a dependency to your project:
+
+    [com.zentrope/web-repl "0.1.0"]
+
+Then, in your actual project, you can start and stop the `web-repl`
+service however you need to in your application.
+
+For example
+
+```clojure
+(ns trepl.core
+  (:require
+    [web-repl.core :as wrepl])
+  (:gen-class))
+
+(defn- on-jvm-shutdown!
+  [f]
+  (doto (Runtime/getRuntime)
+    (.addShutdownHook (Thread. f))))
+
+(defn- hello
+  [msg]
+  (println (str "Hello, " msg "!")))
+
+(defn -main
+  [& args]
+  (println "Hello, World!")
+  (let [state (wrepl/make-wrepl 2012)
+        lock (promise)]
+    (on-jvm-shutdown! (fn [] (wrepl/stop! state)))
+    (on-jvm-shutdown! (fn [] (deliver lock :done)))
+    (wrepl/start! state)
+    (deref lock))))
+```
+
+Surf to `http://localhost:2012` and, in the appropriate "reader" area,
+type:
+
+```clojure
+(ns trepl.core)
+(hello "lovely lady")
+```
+
+and then hit `Shift-Enter`.
 
 ## License
 
--- project.clj
+++ project.clj
@@ -1,31 +1,40 @@
-(defproject web-repl "0.1.0"
-  :description "Experimental app running a repl in the browser to a JVM process."
-  :url "https://github.com/zentrope"
+(defproject com.zentrope/web-repl "0.1.0"
+  :description "Embeddable JVM-hosted web-app enabling a JVM/Clojure repl in a browser."
+  :url "https://github.com/zentrope/web-repl"
   :license {:name "EPL" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ch.qos.logback/logback-classic "1.1.1"]
-                 [compojure "1.1.6"]
-                 [http-kit "2.1.18"]
-                 [hiccup "1.0.5"]
-                 [om "0.5.3"]
-                 [org.clojure/clojure "1.6.0-RC1"]
-                 [org.clojure/clojurescript "0.0-2173"
-                  :exclusions [org.clojure/tools.reader]]
+  :dependencies [[org.clojure/clojure "1.6.0-RC3"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.clojure/tools.nrepl "0.2.3"]
-                 [ring/ring-core "1.2.2"]
-                 [sablono "0.2.14"
-                  :exclusions [org.clojure/tools.reader]]]
-  :plugins [[lein-cljsbuild "1.0.2"]]
-  :cljsbuild {:builds [{:source-paths ["src-cljs"]
-                        :compiler {:output-to "resources/public/main.js"
-                                   :preamble ["react/react.min.js"]
-                                   :externs ["react/externs/react.js"]
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]}
-  :hooks [leiningen.cljsbuild]
+                 [compojure "1.1.6"]
+                 [http-kit "2.1.18"]
+                 [hiccup "1.0.5"]
+                 [ring/ring-core "1.2.2"]]
+  :cljsbuild {:builds []}
+  :scm {:name "git" :url "https://github.com/zentrope/web-repl"}
   :main ^:skip-aot web-repl.core
   :target-path "target/%s"
   :min-lein-version "2.3.4"
   :jvm-opts ["-Dapple.awt.UIElement=true"]
-  :profiles {:uberjar {:aot :all}})
+  :jar-exclusions [#"logback.xml" #".DS_Store"]
+  :aliases {"install!" ^{:doc "Push to local repo"}
+               ["do" "clean"
+                     ["cljsbuild" "clean"]
+                     ["cljsbuild" "once"]
+                     "install"]}
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.2"]]
+                   :cljsbuild {:builds [{:id :dev
+                                         :source-paths ["src-cljs"]
+                                         :compiler {:output-to "resources/public/main.js"
+                                                    :preamble ["react/react.min.js"]
+                                                    :externs ["react/externs/react.js"]
+                                                    :optimizations :whitespace
+                                                    :pretty-print true}}]}
+                   :hooks [leiningen.cljsbuild]
+                   :dependencies [[ch.qos.logback/logback-classic "1.1.1"]
+                                  [om "0.5.3"]
+                                  [org.clojure/clojurescript "0.0-2173"
+                                   :exclusions [org.clojure/tools.reader]]
+                                  [sablono "0.2.14"
+                                   :exclusions [org.clojure/tools.reader]]]}
+             :uberjar {:aot :all}})
